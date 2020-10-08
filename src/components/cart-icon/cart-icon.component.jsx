d896a22fd0c19c498720ed08f4bd6c186d15d071
youngyinghua <67694333+youngyinghua@users.noreply.github.com>
2020-10-08 22:23:18 +0900

    adding cartitem component in cart dropdown and show the itemnumber in carticon component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,15 +4,19 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { toggleCartHidden } from "../../redux/cart/cart.action";
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+const mapState = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((accu, item) => accu + item.quantity, 0),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapState, mapDispatchToProps)(CartIcon);
