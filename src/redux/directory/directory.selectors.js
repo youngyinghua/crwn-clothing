4360b5bc6994763800b7958843da792fa182eba3
youngyinghua <67694333+youngyinghua@users.noreply.github.com>
2020-12-03 22:33:07 +0900

    move code to client folder and add server.js for payment

--- src/redux/directory/directory.selectors.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from "reselect";
-
-const selectDirector = (state) => state.directory;
-export const selectDirectorySections = createSelector(
-  [selectDirector],
-  (directory) => directory.sections
-);
